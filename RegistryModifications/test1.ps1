fe7c43d18ccafac16f4bea77b13a7c24cb79335c
FaronicsProServices <proservices@faronics.com>
2025-09-23 23:45:07 +0530

    Delete RegistryModifications/test1.ps1

--- RegistryModifications/test1.ps1
+++
@@ -1,6 +0,0 @@
-# Set the main profile directory and update Default/Public values
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList" -Name "ProfilesDirectory" -Value "C:\Users"
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList" -Name "Default" -Value "C:\Users\Default"
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList" -Name "Public" -Value "C:\Users\Public"
-
-Write-Host "Main profile paths updated. No SID profile keys were changed."
